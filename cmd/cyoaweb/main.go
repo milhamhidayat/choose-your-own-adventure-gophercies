3a69f81bc951786822c503d216ef408177312e99
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2018-09-27 20:48:07 +0700

    Parse JSON

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	cyoa "choose-your-own-adventure-gophercies"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+)
+
 func main() {
+	filename := flag.String("file", "../../gopher.json", "the JSON file with the CYOA story")
+	flag.Parse()
+	fmt.Printf("Using the story in %s. \n", *filename)
+
+	f, err := os.Open(*filename)
+
+	if err != nil {
+		panic(err)
+	}
+
+	d := json.NewDecoder(f)
+	var story cyoa.Story
+	if err := d.Decode(&story); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v\n", story)
 
 }
